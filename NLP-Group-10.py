9f390b5a7e1179275f8c4c2e694af98f5d63c887
gungorman <gungoremre2004@gmail.com>
2025-10-07 17:16:37 +0200

    Create NLP-Group-10.py

---
+++ NLP-Group-10.py
@@ -0,0 +1,26 @@
+'''Imports'''
+import json
+
+
+'''Pre-Processing'''
+def load_reddit_comment_data(data_directory):
+
+    comments_data = [] # list object that will store the loaded Reddit comments
+
+    # we first open the file that includes our dataset
+    with open(data_directory, 'r', encoding='utf-8') as f:
+        # iterate the file, reading it line by line
+        for line in f:
+            # load the data petraining to a line into a json object in memory
+            data = json.loads(line)
+
+            # append the comment
+            comments_data.append(data['body'])
+
+    # the method returns all the loaded Reddit comments
+    return comments_data
+
+data_dir = r'C:\Users\gungo\OneDrive\Dokumente\GitHub\NLP-Group-10\stocks_comments.ndjson'
+reddit_data = load_reddit_comment_data(data_dir)
+print("Successfully loaded Reddit comments! Our dataset includes %d Reddit comments!" %len(reddit_data))
+print(data_dir)
